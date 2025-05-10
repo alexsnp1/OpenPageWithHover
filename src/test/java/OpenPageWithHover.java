2045d2f0e835b51580e28421c87cef817df6de58
Aleksandr Maliutin <alexander.hroft@gmail.com>
2025-05-10 19:45:44 +0700

    added OpenPageWithHover.java

---
+++ src/test/java/OpenPageWithHover.java
@@ -0,0 +1,33 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class OpenPageWithHover {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1600x900";
+        Configuration.pageLoadStrategy = "eager"; //при долгой загрузке не падает тест
+        Configuration.holdBrowserOpen = true; //не закрывается браузер
+    }
+    @Test
+    void openPageWithHover() {
+        open("https://github.com/");
+        $$("ul li").findBy(Condition.text("Solutions")).hover();
+        $$("ul a").findBy(Condition.text("Enterprises")).click();
+    }
+
+    @Test
+    void dragDrop () {
+        open ("https://the-internet.herokuapp.com/drag_and_drop");
+        actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
+        //$("#column-a").dragAndDropTo($("#column-b"));
+
+    }
+
+}
+
