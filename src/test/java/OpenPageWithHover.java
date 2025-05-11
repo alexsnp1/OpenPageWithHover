282525fdbe9de3bdb95eac20055bc92a8921e9fb
Aleksandr Maliutin <alexander.hroft@gmail.com>
2025-05-11 20:53:08 +0700

    added OpenPageWithHover.java

--- src/test/java/OpenPageWithHover.java
+++ src/test/java/OpenPageWithHover.java
@@ -1,8 +1,10 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
@@ -12,18 +14,24 @@ public class OpenPageWithHover {
     static void beforeAll() {
         Configuration.browserSize = "1600x900";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
     }
     @Test
-    void openPageWithHover() {
+    void openPageWithHoverTest() {
         open("https://github.com/");
-        $$("ul li").findBy(Condition.text("Solutions")).hover();
-        $$("ul a").findBy(Condition.text("Enterprises")).click();
+        $$("ul li").findBy(text("Solutions")).hover();
+        $$("ul a").findBy(text("Enterprises")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AAAI-powered"));
     }
 
     @Test
-    void dragDrop () {
+    void dragDropTest () {
         open ("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
         actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
         //$("#column-a").dragAndDropTo($("#column-b"));
 
     }
