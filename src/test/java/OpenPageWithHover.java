43f379e09b0a2604e8b3a2d1e489ca59986192f2
Aleksandr Maliutin <alexander.hroft@gmail.com>
2025-05-12 23:27:45 +0700

    updated OpenPageWithHover.java

--- src/test/java/OpenPageWithHover.java
+++ src/test/java/OpenPageWithHover.java
@@ -19,17 +19,22 @@ public class OpenPageWithHover {
     @Test
     void openPageWithHoverTest() {
         open("https://github.com/");
+        //Открываем страницы через всплывающие окна
         $$("ul li").findBy(text("Solutions")).hover();
         $$("ul a").findBy(text("Enterprises")).click();
+        //роверка что мы на нужной странице
         $("#hero-section-brand-heading").shouldHave(text("The AAAI-powered"));
     }
 
     @Test
     void dragDropTest () {
         open ("https://the-internet.herokuapp.com/drag_and_drop");
+        //проверка перед тестом
         $("#column-a").shouldHave(text("A"));
         $("#column-b").shouldHave(text("B"));
+        //перетаскиваем элемент к другому элементу
         actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
+        //проверка после теста
         $("#column-a").shouldHave(text("B"));
         $("#column-b").shouldHave(text("A"));
         //$("#column-a").dragAndDropTo($("#column-b"));
